a8cad3327c78c7ca6318472e01e05b00aa72cb08
Flourine <trananhtuan2005dh@gmail.com>
2024-11-05 09:34:10 +0700

    mvc modals

--- src/server.js
+++ src/server.js
@@ -1,27 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
-
-
+const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./routes/web');
 
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
-
-//config template 
-app.set('views',path.join(__dirname,'views/'));
-app.set('view engine', 'ejs');
+//config template engine
+configViewEngine(app);
 
 //khai báo route
-app.get('/', (req,res)=>{
-    res.send('Hello World!');
-});
-
-app.get('/abc', (req,res)=>{
-    // res.send('<h1>fuck you</h1>');
-    res.render('sample.ejs');
-});
+app.use('/test',webRoutes);
 
 app.listen(port, ()=>{
     console.log(`Example app listening on port ${port}`);
